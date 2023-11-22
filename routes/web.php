4227361c49eda49f4db912dac4f4dcac50d19bfc
Wedwin02 <navarretee210@gmail.com>
2023-11-22 01:13:00 -0600

    Modificacion de perfil

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,14 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+/// admin group middleware
 Route::middleware(['auth','role:admin'])->group(function(){
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    
 });
 
 Route::middleware(['auth','role:agent'])->group(function(){
@@ -41,3 +46,4 @@ Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
 
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
