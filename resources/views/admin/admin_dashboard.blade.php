dc584085c31a9428453e6fce9440ae765963396f
Wedwin02 <navarretee210@gmail.com>
2023-12-02 03:06:30 -0600

    Modificacion de CRUD Property Type Successfully

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -10,6 +10,10 @@
 
 	<title>Admin Dashboard</title>
 
+	<!-- Plugin css for this page -->
+	<link rel="stylesheet" href="{{asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css')}}">
+	<!-- End plugin css for this page -->
+
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -36,32 +40,9 @@
   <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.png')}}" />
 
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>https://cdnjs.com/libraries/toastr.js
 
-  <script>
-   @if(Session::has('message')){
-   		var type = "{{ Session::get('alert-type','info') }}";
-		switch(type){
-			case 'info':
-			toastr.info(" {{ Session::get('message') }} ");
-			break;
-		
-			case 'success':
-			toastr.success(" {{ Session::get('message') }} ");
-			break;
-		
-			case 'warning':
-			toastr.warning(" {{ Session::get('message') }} ");
-			break;
-		
-			case 'error':
-			toastr.error(" {{ Session::get('message') }} ");
-			break; 
-		}
-   }
-   @endif 
-  </script>
 </head>
+
 <body>
 	<div class="main-wrapper">
 
@@ -83,6 +64,7 @@
 		</div>
 	</div>
 
+
 	<!-- core:js -->
 	<script src="{{asset('backend/assets/vendors/core/core.js')}}"></script>
 	<!-- endinject -->
@@ -97,9 +79,44 @@
 	<script src="{{asset('backend/assets/js/template.js')}}"></script>
 	<!-- endinject -->
 
+
 	<!-- Custom js for this page -->
   <script src="{{asset('backend/assets/js/dashboard-dark.js')}}"></script>
 	<!-- End custom js for this page -->
 
+		<!-- Plugin js for this page -->
+		<script src="{{asset('backend/assets/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+		<script src="{{asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js')}}"></script>
+		  <!-- End plugin js for this page -->
+		  <!-- Custom js for this page -->
+		<script src="{{asset('backend/assets/js/data-table.js')}}"></script>
+		<!-- End custom js for this page -->
+		  
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+	<script src="{{asset('backend/assets/js/code/code.js')}}"></script>
+	<script>
+	  @if(Session::has('message')){
+			  var type = "{{ Session::get('alert-type','info') }}";
+		   switch(type){
+			   case 'info':
+			   toastr.info(" {{ Session::get('message') }} ");
+			   break;
+		   
+			   case 'success':
+			   toastr.success(" {{ Session::get('message') }} ");
+			   break;
+		   
+			   case 'warning':
+			   toastr.warning(" {{ Session::get('message') }} ");
+			   break;
+		   
+			   case 'error':
+			   toastr.error(" {{ Session::get('message') }} ");
+			   break; 
+		   }
+	  }
+	  @endif 
+	 </script>
 </body>
 </html>    
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend\PropertyTypeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -53,3 +53,19 @@ Route::middleware(['auth','role:agent'])->group(function(){
 });
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+Route::middleware(['auth','role:admin'])->group(function(){
+
+    ///GET
+    
+    Route::controller(PropertyTypeController::class)->group(function(){
+        Route::get('/all/type', 'AllType')->name('all.type');
+        Route::get('/add/type', 'AddType')->name('add.type');
+        Route::post('/store/type', 'StoreType')->name('store.type');
+        Route::get('/edit/type/{id}', 'EditType')->name('edit.type');
+        Route::post('/update/type', 'UpdateType')->name('update.type');
+        Route::get('/delete/type/{id}', 'DeleteType')->name('delete.type');
+    });
+
+    
+});
